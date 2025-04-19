6de678c281afb40d0ea3cfe7be3d720c38252836
Gustavo Gomes <gustavoalang13@gmail.com>
2025-04-19 12:21:45 -0300

    Criação dos casos de teste e dos testes de edição de usuários

---
+++ cypress/fixtures/usuariosTeste.js
@@ -0,0 +1,21 @@
+export const usuarios = {
+    usuario1: {
+        nome: "Michael Scott",
+        email: "michael.g.scott@teste.com",
+        password: "michael@TheOffice.1234567890",
+        admUsuario: true
+    },
+    usuario2: {
+        nome: "Dwight Schrute",
+        email: "dwight.k.schrute@teste.com",
+        password: "dwight@TheOffice.1234567890",
+        admUsuario: false
+    },
+    usuarioEditado: {
+        nome: "Michael Editado",
+        email: "michael.editado.scott@teste.com",
+        password: "michael@Editado.1234567890",
+        admUsuario: false
+    }
+}
+
